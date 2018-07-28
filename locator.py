17b80ab5efb69a01801b9bea724bc068d3b18dc8
sennasemakula <sennasemakula@hotmail.co.uk>
2018-07-28 20:21:07 +0100

    Created a class to identify the nearest ATM via post codes

--- locator.py
+++ locator.py
@@ -1,20 +1,34 @@
 """This class allows users to enter their location"""
+import requests
+import json
 
 class Locator():
+	"""Class that identifies the nearest ATM for users"""
 
 	def __init__(self, location):
 		self.location = location
+		self.first_code = ''
+		self.second_code = ''
 
 
 	def prompt_location(self):
-		post_code = input("Enter your post code")
+		self.first_code = input("Enter first part of your post code: ")
+		self.second_code = input("Enter second part of your post code: ")
+
+		postcode = first + second
 
 		return post_code
 
 
 	def find_location (self, post_code):
-		sel = input("Enter post code: ")
+		post_code = self.prompt_location()
 
-		p
+		#Look up post code from a list of API's
+		url = 'maps.googleapis.com/maps/api/geocode/json?=' + self.first + '+' + self.second_code
+		print(url)
+		response = request.get(url)
+
+		data = response.json()
+
 
 
