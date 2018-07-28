2899a02ba0c3e7386ec656a6f0b7904832c088ab
sennasemakula <sennasemakula@hotmail.co.uk>
2018-07-28 19:37:17 +0100

    Exception handling updated for long/lat values

---
+++ locator.py
@@ -0,0 +1,20 @@
+"""This class allows users to enter their location"""
+
+class Locator():
+
+	def __init__(self, location):
+		self.location = location
+
+
+	def prompt_location(self):
+		post_code = input("Enter your post code")
+
+		return post_code
+
+
+	def find_location (self, post_code):
+		sel = input("Enter post code: ")
+
+		p
+
+
